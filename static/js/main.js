6650b7c13de8383f1e5bb13864a79fb8078f413c
Carlos Gomez <carlosgomez.deb@gmail.com>
2015-06-02 17:46:11 -0500

    Agregado ocultar menu on click

--- static/js/main.js
+++ static/js/main.js
@@ -18,39 +18,11 @@ function main () {
 		}
 	});
 
-	if (contador = 1){
-		myEl.click({
-			$('nav').animate({
-				left: '-100%'
-			});
-		})
-	}
+//Hide on click <li>
+	$('.Mainmenu-itemLink').click(function(){
+		$('nav').animate({
+			left: '-100%'
+		});
+	});
 }
 
-
-/*$(document).ready(main);
-
-var contador = 1;
-
-function main () {
-	$('.menu_bar').click(function(){
-		if (contador == 1) {
-			$('nav').animate({
-				left: '0'
-			});  //fin de la instruccion if
-			contador = 0;
-		} else if {
-			contador = 0;
-			$('nav').animate({
-				left: '-100%'
-			});  //fin de la instruccion else if
-		} else {
-			$('.Mainmenu-itemLink').click(function(){
-					$('nav').animate({
-						left: '-100%'
-					});
- 						//modificar este valor si no queda
-			});
-		} // fin del else
-	}); //fin del main menu_bar
-}*/
