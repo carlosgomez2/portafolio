1063a1d0c1a830d2b7ec324f368aa4063d077e35
Carlos Gomez <carlosgomez.deb@gmail.com>
2015-06-02 16:41:51 -0500

    Modificacion al menu y main js

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(main);
  
-var contador = 1;
+var contador = 1,
+	myEl = document.getElementsByClassName("Mainmenu-itemLink");
  
 function main () {
 	$('.menu_bar').click(function(){
@@ -16,10 +17,40 @@ function main () {
 			});
 		}
 	});
- 
-	// Mostramos y ocultamos submenus
-	$('.submenu').click(function(){
-		$(this).children('.children').slideToggle();
-	});
+
+	if (contador = 1){
+		myEl.click({
+			$('nav').animate({
+				left: '-100%'
+			});
+		})
+	}
 }
 
+
+/*$(document).ready(main);
+
+var contador = 1;
+
+function main () {
+	$('.menu_bar').click(function(){
+		if (contador == 1) {
+			$('nav').animate({
+				left: '0'
+			});  //fin de la instruccion if
+			contador = 0;
+		} else if {
+			contador = 0;
+			$('nav').animate({
+				left: '-100%'
+			});  //fin de la instruccion else if
+		} else {
+			$('.Mainmenu-itemLink').click(function(){
+					$('nav').animate({
+						left: '-100%'
+					});
+ 						//modificar este valor si no queda
+			});
+		} // fin del else
+	}); //fin del main menu_bar
+}*/
